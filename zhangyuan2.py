a2ccfaf3430961ded1364861991da0e6599a7410
zhangyuan1001 <missyuan1001@gmail.com>
2024-11-04 17:10:47 +0300

    Add files via upload

---
+++ zhangyuan2.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from skimpy import skim
+df=pd.read_csv("shujuji2(1).csv")
+skim(df)
+print(df.head())
+print(df.describe())
+print(df.isnull().sum())
+print(df.dtypes)
+import matplotlib.pyplot as plt # type: ignore
+survival_counts=df['Survived'].value_counts()
+survival_counts.plot(kind='bar')
+plt.title('Survival Counts')
+plt.xlabel('Survived')
+plt.ylabel('Count')
+plt.xticks(ticks=[0,1],labels=['No','Yes'],rotation=0)
+plt.show()
+gender_survival=df.groupby(['Sex','Survived']).size().unstack()
+gender_survival.plot(kind='bar',stacked=True)
+plt.title('Survival by Gender')
+plt.xlabel('Gender')
+plt.ylabel('Count')
+plt.xticks(rotation=0)
+plt.show()
